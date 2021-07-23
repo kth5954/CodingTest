f496adee269333990e45cc591dcb7263126a14b4
kth5954 <kth5954@gmail.com>
2021-07-23 15:16:11 +0900

    15649

---
+++ sort/n&m_15649.py
@@ -0,0 +1,19 @@
+N, M = map(int, input().split())
+visited = [False]*N
+out = []
+
+
+def solve(depth, n, m):
+    if depth == m:
+        print(" ".join(map(str, out)))
+        return
+    for i in range(len(visited)):
+        if visited[i] is False:
+            visited[i] = True
+            out.append(i+1)
+            solve(depth+1, n, m)
+            visited[i] = False
+            out.pop()
+
+solve(0, N, M)
+n
