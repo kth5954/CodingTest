64f3f2a65a4633ce3224328b631a13a617b34055
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-08-05 14:59:33 +0900

    2022.08.05: programmers hash lv2

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os
+
+
+def main():
+    print(solution(["119", "97674223", "1195524421"]))
+    print(solution(["123", "456", "789"]))
+    print(solution(["12", "123", "1235", "567", "88"]))
+
+    arr = ['32','123', '1234','214', '12', '21', '321', '32112']
+    print(sorted(arr))
+    return 0
+
+
 def solution(phone_book):
-    answer = True
-    return answer
+    array = sorted(phone_book)
+    for i in range(1, len(array)):
+        if array[i-1] == array[i][:len(array[i-1])]:
+            return False
+    return True
 
 
+
+if __name__ == "__main__":
+    main()
 
